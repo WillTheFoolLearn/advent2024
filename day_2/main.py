1e37dc959460fbfc37460a97299c555c3cd4db63
willthefoollearn <willthefoollearn@gmail.com>
2024-12-03 10:36:35 -0800

    Day 2 Part 1 complete

--- day_2/main.py
+++ day_2/main.py
@@ -1,15 +1,15 @@
 def main():
-    file = open("test.txt", "r")
+    file = open("input.txt", "r")
     full_list = file.readlines()
     total = 0
-    state = 0
 
     for report in full_list:
         levels = list(map(int, report.split()))
+        state = 0
 
         for i in range(len(levels) - 1):
             if levels[i] == levels[i + 1]:
-                continue
+                break
             
             if state == 0:
                 if levels[i] > levels[i + 1]:
@@ -19,15 +19,15 @@ def main():
             else:
                 if state == 1:
                     if levels[i] < levels[i + 1]:
-                        continue
+                        break
                 else:
-                    if levels[i] < levels[i + 1]:
-                        continue
+                    if levels[i] > levels[i + 1]:
+                        break
             
-            if not (-4 < levels[i] - levels[i + 1] < 4):
-                continue
+            if not (-4 < (levels[i] - levels[i + 1]) < 4):
+                break
 
-            if i == (len(levels) - 1):
+            if i == (len(levels) - 2):
                 total += 1
             
     print(total)
