7478af220f6f4b26d5bd00c0a7dc6d2d294b9c21
willthefoollearn <willthefoollearn@gmail.com>
2024-12-04 08:55:03 -0800

    Day 2 complete

--- day_2/main.py
+++ day_2/main.py
@@ -5,72 +5,42 @@ def main():
 
     for report in full_list:
         levels = list(map(int, report.split()))
-        state = 0
 
-        for i in range(len(levels) - 1):
-            if levels[i] == levels[i + 1]:
-                del levels[i]
-                if check_tolerance(levels):
-                    total +=1
-                break
-            
-            if state == 0:
-                if levels[i] > levels[i + 1]:
-                    state = 1
-                else:
-                    state = 2
+        for i in range(1, len(levels) - 1):
+            if levels[i-1] < levels[i] < levels[i+1] or levels[i-1] > levels[i] > levels[i+1]:
+                if abs(levels[i-1] -  levels[i]) > 3 or abs(levels[i] -  levels[i + 1]) > 3:
+                    if not remove_elem(levels, i):
+                        break
             else:
-                if state == 1:
-                    if levels[i] < levels[i + 1]:
-                        del levels[i]
-                        if check_tolerance(levels):
-                            total +=1
-                        break
-                else:
-                    if levels[i] > levels[i + 1]:
-                        del levels[i]
-                        if check_tolerance(levels):
-                            total +=1
-                        break
-            
-            if not (-4 < (levels[i] - levels[i + 1]) < 4):
-                del levels[i]
-                if check_tolerance(levels):
-                    total +=1
-                break
+                if not remove_elem(levels, i):
+                    break
 
-            if i == (len(levels) - 2):
+            if i == len(levels) - 2:
                 total += 1
-            
+
     print(total)
 
-    return total
+def remove_elem(lvl, idx):
+    lvl_l = lvl[:]
+    lvl_m = lvl[:]
+    lvl_r = lvl[:]
+    del lvl_l[idx - 1]
+    del lvl_m[idx]
+    del lvl_r[idx + 1]
 
-def check_tolerance(short_list):
-    state = 0
-    for i in range(len(short_list) - 1):
-        if short_list[i] == short_list[i + 1]:
-            break
-        
-        if state == 0:
-            if short_list[i] > short_list[i + 1]:
-                state = 1
-            else:
-                state = 2
+    return check_tolerance(lvl_l) or check_tolerance(lvl_m) or check_tolerance(lvl_r)
+
+def check_tolerance(lvl):
+    for i in range(1, len(lvl) - 1):
+        if lvl[i-1] < lvl[i] < lvl[i+1] or lvl[i-1] > lvl[i] > lvl[i+1]:
+            if abs(lvl[i-1] -  lvl[i]) > 3 or abs(lvl[i] -  lvl[i + 1]) > 3:
+                break
         else:
-            if state == 1:
-                if short_list[i] < short_list[i + 1]:
-                    break
-            else:
-                if short_list[i] > short_list[i + 1]:
-                    break
-        
-        if not (-4 < (short_list[i] - short_list[i + 1]) < 4):
             break
 
-        if i == (len(short_list) - 2):
+        if i == len(lvl) - 2:
             return True
-
+    
     return False
 
 if __name__ == "__main__":
